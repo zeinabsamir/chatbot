796f145674d609145d81434de0674859b6227abc
zeinab <zeinabsamir8@gmail.com>
2018-05-12 04:54:38 +0200

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+
+
+app.set('port', (process.env.PORT || 8080));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+     res.send('hello this is a chatbot')
+})
+
+app.get('/webhook/', function(req, res) {
+    if(req.query['hub.verify_token'] === 'zeinab123') {
+        res.send(req.query['hub.challeng'])
+    } else {
+        res.send('No entry');
+    }
+      
+})
+
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port') );
+})
