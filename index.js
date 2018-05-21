b6cdad13a6b8774bc697bc8a5aa7888d5aaa8275
zeinab <zeinabsamir8@gmail.com>
2018-05-21 12:41:19 +0200

    generic template

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.get('/webhook/', function(req, res) {
     }
       
 })
+
 app.post('/webhook', async (req, res) => {  
   console.log(req.body);
   let messaging_events = req.body.entry[0].messaging
@@ -65,10 +66,40 @@ function sendText(sender, text) {
     "method": "POST",
     "json": {  
         "recipient": {"id": sender},
-         "message": messageData,
-         "messaging_type": "MESSAGE_TAG",
-         "tag": "SHIPPING_UPDATE"
-    }
+         "message": {
+          "attachment":{
+            "type":"template",
+            "payload":{
+              "template_type":"generic",
+              "elements":[
+                 {
+                  "title":"Welcome!",
+                  "image_url":"https://petersfancybrownhats.com/company_image.png",
+                  "subtitle":"We have the right hat for everyone.",
+                  "default_action": {
+                    "type": "web_url",
+                    "url": "https://petersfancybrownhats.com/view?item=103",
+                    "messenger_extensions": false,
+                    "webview_height_ratio": "tall",
+                    "fallback_url": "https://petersfancybrownhats.com/"
+                  },
+                  "buttons":[
+                    {
+                      "type":"web_url",
+                      "url":"https://petersfancybrownhats.com",
+                      "title":"View Website"
+                    },{
+                      "type":"postback",
+                      "title":"Start Chatting",
+                      "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                    }              
+                  ]      
+                }
+              ]
+            }
+          }
+        }  
+      }
   }, (err, res, body) => {
     if (!err) {
       console.log('message sent!')
