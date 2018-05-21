92ca060a519c024cbef81952aa4a1742b961b73c
zeinab <zeinabsamir8@gmail.com>
2018-05-21 13:49:22 +0200

    list template

--- index.js
+++ index.js
@@ -70,64 +70,34 @@ function sendText(sender, text) {
           "attachment": {
             "type": "template",
             "payload": {
-              "template_type": "list",
-              "top_element_style": "compact",
-              "elements": [
+              sharable: true,
+              aspectRatio: 'horizontal',
+              elements: [
                 {
-                  "title": "Classic T-Shirt Collection",
-                  "subtitle": "See all our colors",
-                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
-                  "buttons": [
+                  title: 'my title',
+                  subtitle: 'I am a sub title',
+                  imageUrl: 'http://image.server.com/cover.png',
+                  buttons: [
                     {
-                      "title": "View",
-                      "type": "web_url",
-                      "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                      "messenger_extensions": true,
-                      "webview_height_ratio": "tall",
-                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                      type: 'url',
+                      url: 'http://javascript-jedi.com',
+                      label: 'Javascript Jedi'
                     }
                   ]
                 },
                 {
-                  "title": "Classic White T-Shirt",
-                  "subtitle": "See all our colors",
-                  "default_action": {
-                    "type": "web_url",
-                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                    "messenger_extensions": false,
-                    "webview_height_ratio": "tall"
-                  }
-                },
-                {
-                  "title": "Classic Blue T-Shirt",
-                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                  "subtitle": "100% Cotton, 200% Comfortable",
-                  "default_action": {
-                    "type": "web_url",
-                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                    "messenger_extensions": true,
-                    "webview_height_ratio": "tall",
-                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                  },
-                  "buttons": [
+                  title: 'another title',
+                  subtitle: 'Second sub title',
+                  imageUrl: 'http://image.server.com/cover2.png',
+                  buttons: [
                     {
-                      "title": "Shop Now",
-                      "type": "web_url",
-                      "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                      "messenger_extensions": true,
-                      "webview_height_ratio": "tall",
-                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
+                      type: 'url',
+                      url: 'http://javascript-jedi.com',
+                      label: 'Javascript Jedi'
                     }
-                  ]        
+                  ]
                 }
-              ],
-               "buttons": [
-                {
-                  "title": "View More",
-                  "type": "postback",
-                  "payload": "payload"            
-                }
-              ]  
+              ]      
             }
           }
         }
