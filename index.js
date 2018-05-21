b917b9c0342387534812efdd79d451f5ea1abd82
zeinab <zeinabsamir8@gmail.com>
2018-05-22 00:06:48 +0200

    open gragh

--- index.js
+++ index.js
@@ -75,18 +75,18 @@ function sendText(sender, text) {
                  {
                   "url":"https://open.spotify.com/track/7GhIk7Il098yCjg4BQjzvb",
                   "buttons":[
-                    {
-                      "type":"web_url",
-                      "url":"https://en.wikipedia.org/wiki/Rickrolling",
-                      "title":"View More"
-                    }              
-                  ]      
-                }
+                   {
+                  "type":"web_url",
+                  "url":"https://www.messenger.com",
+                  "title":"Visit Messenger"
+                  }
               ]
             }
+          ]
              
           }
         }
+      }
       }    
             
   }, (err, res, body) => {
