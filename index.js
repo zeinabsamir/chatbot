ad079f27f99dc91b4b5be758441ec1bd6ba91bbd
zeinab <zeinabsamir8@gmail.com>
2018-05-22 10:29:17 +0200

    airline boarding pass template

--- index.js
+++ index.js
@@ -70,29 +70,69 @@ function sendText(sender, text) {
           "attachment":{
             "type":"template",
             "payload": {
-              "template_type":"generic",
-              "elements":[
-                 {
-                  "title":"Welcome!",
-                  "image_url":"https://petersfancybrownhats.com/company_image.png",
-                  "subtitle":"We have the right hat for everyone.",
-                  "buttons":[
+              "template_type": "airline_boardingpass",
+              "intro_message": "You are checked in.",
+              "locale": "en_US",
+              "boarding_pass": [
+                {
+                  "passenger_name": "SMITH\/NICOLAS",
+                  "pnr_number": "CG4X7U",
+                  "seat": "74J",            
+                  "logo_image_url": "https:\/\/www.example.com\/en\/logo.png",
+                  "header_image_url": "https:\/\/www.example.com\/en\/fb\/header.png",
+                  "qr_code": "M1SMITH\/NICOLAS  CG4X7U nawouehgawgnapwi3jfa0wfh",
+                  "above_bar_code_image_url": "https:\/\/www.example.com\/en\/PLAT.png",
+                  "auxiliary_fields": [
                     {
-                      "type":"web_url",
-                      "url":"https://petersfancybrownhats.com",
-                      "title":"View Website"
-                    },{
-                      "type":"postback",
-                      "title":"Start Chatting",
-                      "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                    }              
-                  ]      
+                      "label": "Terminal",
+                      "value": "T1"
+                    },
+                    {
+                      "label": "Departure",
+                      "value": "30OCT 19:05"
+                    }
+                  ],
+                  "secondary_fields": [
+                    {
+                      "label": "Boarding",
+                      "value": "18:30"
+                    },
+                    {
+                      "label": "Gate",
+                      "value": "D57"
+                    },
+                    {
+                      "label": "Seat",
+                      "value": "74J"
+                    },
+                    {
+                      "label": "Sec.Nr.",
+                      "value": "003"
+                    }
+                  ],
+                  "flight_info": {
+                    "flight_number": "KL0642",
+                    "departure_airport": {
+                      "airport_code": "JFK",
+                      "city": "New York",
+                      "terminal": "T1",
+                      "gate": "D57"
+                    },
+                    "arrival_airport": {
+                      "airport_code": "AMS",
+                      "city": "Amsterdam"
+                    },
+                    "flight_schedule": {
+                      "departure_time": "2016-01-02T19:05",
+                      "arrival_time": "2016-01-05T17:30"
+                    }
+                  }
                 }
               ]
             }
-          }  
+          }
         }
-      }       
+      }          
             
   }, (err, res, body) => {
     if (!err) {
