7df0ef8a73e963b941c77c2707e5497ade8a9eac
zeinab <zeinabsamir8@gmail.com>
2018-05-22 11:24:54 +0200

    button template

--- index.js
+++ index.js
@@ -70,128 +70,49 @@ function sendText(sender, text) {
           "attachment":{
             "type":"template",
             "payload": {
-              "template_type": "airline_itinerary",
-              "intro_message": "Here is your flight itinerary.",
-              "locale": "en_US",
-              "pnr_number": "ABCDEF",
-              "passenger_info": [
+              "template_type":"generic",
+              "elements":[
                 {
-                  "name": "Farbound Smith Jr",
-                  "ticket_number": "0741234567890",
-                  "passenger_id": "p001"
-                },
-                {
-                  "name": "Nick Jones",
-                  "ticket_number": "0741234567891",
-                  "passenger_id": "p002"
-                }
-              ],
-              "flight_info": [
-                {
-                  "connection_id": "c001",
-                  "segment_id": "s001",
-                  "flight_number": "KL9123",
-                  "aircraft_type": "Boeing 737",
-                  "departure_airport": {
-                    "airport_code": "SFO",
-                    "city": "San Francisco"
-                  },
-                  "arrival_airport": {
-                    "airport_code": "SLC",
-                    "city": "Salt Lake City"
-                  },
-                  "flight_schedule": {
-                    "departure_time": "2016-01-02T19:45",
-                    "arrival_time": "2016-01-02T21:20"
-                  },
-                  "travel_class": "business"
-                },
-                {
-                  "connection_id": "c002",
-                  "segment_id": "s002",
-                  "flight_number": "KL321",
-                  "aircraft_type": "Boeing 747-200",
-                  "travel_class": "business",
-                  "departure_airport": {
-                    "airport_code": "SLC",
-                    "city": "Salt Lake City",
-                    "terminal": "T1",
-                    "gate": "G33"
-                  },
-                  "arrival_airport": {
-                    "airport_code": "AMS",
-                    "city": "Amsterdam",
-                    "terminal": "T1",
-                    "gate": "G33"
-                  },
-                  "flight_schedule": {
-                    "departure_time": "2016-01-02T22:45",
-                    "arrival_time": "2016-01-03T17:20"
-                  }
-                }
-              ],
-              "passenger_segment_info": [
-                {
-                  "segment_id": "s001",
-                  "passenger_id": "p001",
-                  "seat": "12A",
-                  "seat_type": "Business"
-                },
-                {
-                  "segment_id": "s001",
-                  "passenger_id": "p002",
-                  "seat": "12B",
-                  "seat_type": "Business"
-                },
-                {
-                  "segment_id": "s002",
-                  "passenger_id": "p001",
-                  "seat": "73A",
-                  "seat_type": "World Business",
-                  "product_info": [
+                  "title":"Breaking News: Record Thunderstorms",
+                  "subtitle":"The local area is due for record thunderstorms over the weekend.",
+                  "image_url":"https://thechangreport.com/img/lightning.png",
+                  "buttons": [
                     {
-                      "title": "Lounge",
-                      "value": "Complimentary lounge access"
-                    },
-                    {
-                      "title": "Baggage",
-                      "value": "1 extra bag 50lbs"
-                    }
-                  ]
-                },
-                {
-                  "segment_id": "s002",
-                  "passenger_id": "p002",
-                  "seat": "73B",
-                  "seat_type": "World Business",
-                  "product_info": [
-                    {
-                      "title": "Lounge",
-                      "value": "Complimentary lounge access"
-                    },
-                    {
-                      "title": "Baggage",
-                      "value": "1 extra bag 50lbs"
+                      "type": "element_share",
+                      "share_contents": { 
+                        "attachment": {
+                          "type": "template",
+                          "payload": {
+                            "template_type": "generic",
+                            "elements": [
+                              {
+                                "title": "I took the hat quiz",
+                                "subtitle": "My result: Fez",
+                                "image_url": "https://bot.peters-hats.com/img/hats/fez.jpg",
+                                "default_action": {
+                                  "type": "web_url",
+                                  "url": "http://m.me/petershats?ref=invited_by_24601"
+                                },
+                                "buttons": [
+                                  {
+                                    "type": "web_url",
+                                    "url": "http://m.me/petershats?ref=invited_by_24601", 
+                                    "title": "Take Quiz"
+                                  }
+                                ]
+                              }
+                            ]
+                          }
+                        }
+                      }
                     }
                   ]
                 }
-              ],
-              "price_info": [
-                {
-                  "title": "Fuel surcharge",
-                  "amount": "1597",
-                  "currency": "USD"
-                }
-              ],
-              "base_price": "12206",
-              "tax": "200",
-              "total_price": "14003",
-              "currency": "USD"
+              ]
             }
           }
-        }
-      }          
-            
+        }            
+      }      
   }, (err, res, body) => {
     if (!err) {
       console.log('message sent!')
