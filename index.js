d4d9ec600c06158b6d154d15fc56ff6e54c12383
zeinab <zeinabsamir8@gmail.com>
2018-05-22 09:48:53 +0200

    generic template

--- index.js
+++ index.js
@@ -76,14 +76,17 @@ function sendText(sender, text) {
                   "title":"Welcome!",
                   "image_url":"https://petersfancybrownhats.com/company_image.png",
                   "subtitle":"We have the right hat for everyone.",
-                  "default_action": {
-                    "type": "web_url",
-                    "url": "https://petersfancybrownhats.com/view?item=103",
-                    "messenger_extensions": false,
-                    "webview_height_ratio": "tall",
-                    "fallback_url": "https://petersfancybrownhats.com/"
-                  }
-                  
+                  "buttons":[
+                    {
+                      "type":"web_url",
+                      "url":"https://petersfancybrownhats.com",
+                      "title":"View Website"
+                    },{
+                      "type":"postback",
+                      "title":"Start Chatting",
+                      "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                    }              
+                  ]      
                 }
               ]
             }
