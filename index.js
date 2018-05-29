8f1b6d52cd86ad9bb8f75fea36d72eef889fc81c
zeinab <zeinabsamir8@gmail.com>
2018-05-30 01:15:41 +0200

    arabic

--- index.js
+++ index.js
@@ -256,7 +256,7 @@ app.post('/webhook', async (req, res) => {
 })
 function decideMessage(sender, text1) {
   let text = text1.toLowerCase();
-  if (text.includes('get started') || text.includes('ابدا من جديد')) {
+  if (text.includes('get started') ) {
     sendText(sender, " {{user_first_name}}ازيك يا ");
     sendText(sender, "اهلا بيك في بوتس بالعربي اول منصه عربيه متخصصه في الكتابه عن البوتس باللغه العربيه ستجد انواع مختلفه من المحتوى في بوتس بالعربي");
     sendText(sender, "محتوى تعليمي لبناء البوتس على منصات المراسله المختلفه(ماسنجر,تليجرام,سلاك وغيرها");
