bea7ae12a06afb56b6314ed75b17e360f9cbfca9
zeinab <zeinabsamir8@gmail.com>
2018-05-30 01:28:54 +0200

    arabic

--- index.js
+++ index.js
@@ -261,8 +261,8 @@ function decideMessage(sender, text1) {
    // sendText(sender, "اهلا بيك في بوتس بالعربي اول منصه عربيه متخصصه في الكتابه عن البوتس باللغه العربيه ستجد انواع مختلفه من المحتوى في بوتس بالعربي");
    // sendText(sender, "محتوى تعليمي لبناء البوتس على منصات المراسله المختلفه(ماسنجر,تليجرام,سلاك وغيرها");
    // sendText(sender, "كيف يمكن ان نساعدك؟");
-    genericMassge(sender);
-  
+  //  genericMassge(sender);
+  sendImage(sender);
   } else if (text.includes('page')) {
     sendPageURL(sender);
   }
