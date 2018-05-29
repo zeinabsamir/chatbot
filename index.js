65a8c3542868ad48afb902b82060153401963cd7
zeinab <zeinabsamir8@gmail.com>
2018-05-30 01:20:51 +0200

    arabic

--- index.js
+++ index.js
@@ -256,12 +256,12 @@ app.post('/webhook', async (req, res) => {
 })
 function decideMessage(sender, text1) {
   let text = text1.toLowerCase();
-  if (text.includes('get started') ) {
+  if (text.includes('getstarted') || text.includes('ابدا من جديد')) {
     sendText(sender, " {{user_first_name}}ازيك يا ");
-    //sendText(sender, "اهلا بيك في بوتس بالعربي اول منصه عربيه متخصصه في الكتابه عن البوتس باللغه العربيه ستجد انواع مختلفه من المحتوى في بوتس بالعربي");
-   // sendText(sender, "محتوى تعليمي لبناء البوتس على منصات المراسله المختلفه(ماسنجر,تليجرام,سلاك وغيرها");
-   // sendText(sender, "كيف يمكن ان نساعدك؟");
-   // genericMassge(sender);
+    sendText(sender, "اهلا بيك في بوتس بالعربي اول منصه عربيه متخصصه في الكتابه عن البوتس باللغه العربيه ستجد انواع مختلفه من المحتوى في بوتس بالعربي");
+    sendText(sender, "محتوى تعليمي لبناء البوتس على منصات المراسله المختلفه(ماسنجر,تليجرام,سلاك وغيرها");
+    sendText(sender, "كيف يمكن ان نساعدك؟");
+    genericMassge(sender);
     sendQuickReplies(sender);
   } else if (text.includes('page')) {
     sendPageURL(sender);
