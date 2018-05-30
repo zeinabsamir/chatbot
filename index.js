78b3323badb977a7a9df47279520629520bc7947
zeinab <zeinabsamir8@gmail.com>
2018-05-30 15:31:48 +0200

    promise

--- index.js
+++ index.js
@@ -471,7 +471,7 @@ function requestProfile(messageData){
 },
 function (error, response, body) {
     if (!error && response.statusCode == 200) {
-       res.send(body);
+       response.send(body);
        console.log(body);
     } else { 
       console.error("Unable to send message:" + error);
